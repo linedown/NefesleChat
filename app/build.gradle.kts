8c13bcf3ec1aca83e4896b7a24cd66388ecbc67d
linedown <puppi2010@mail.ru>
2025-05-13 16:36:00 +0300

    Исправление отступов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4"
+        versionName = "0.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
