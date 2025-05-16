d4aec90c669269545aab9fe03d4e0eb55e91c46f
linedown <puppi2010@mail.ru>
2025-05-16 21:28:43 +0300

    Версия 0.5: задачи и заметки сделаны

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.45"
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
