85e5e3139c13bea292ae0e608c5244afcae944c6
linedown <puppi2010@mail.ru>
2025-05-17 22:46:32 +0300

    Версия 0.6. Расписание готово

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.5"
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
